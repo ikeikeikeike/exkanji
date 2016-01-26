c2bbeaaa819c92b340e86c081ba5af8355481b89
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-01-26 13:22:44 +0900

    Added keeps white spaces and U+3000(IDEOGRAPHIC SPACE)

--- lib/exkanji/translator.ex
+++ lib/exkanji/translator.ex
@@ -28,22 +28,16 @@ defmodule Exkanji.Translator do
     |> Exromaji.romaji
   end
 
-  # TODO: replace white space to U+3000(IDEOGRAPHIC SPACE)
   defp convert(text) do
-    text
-    |> String.split
-    |> Enum.map(fn(t) ->
-      case Mecab.parse(t) do
-        # {:error, _reason} -> nil
-        {:ok, lines} ->
-          lines
-          |> Enum.map(fn(l) ->
-            if l.read, do: l.read, else: l.surface
-          end)
-          |> Enum.join
-      end
-    end)
-    |> Enum.join(" ")
+    case Mecab.parse(String.replace(text, " ", "{{__}}")) do
+      # {:error, _reason} -> nil
+      {:ok, lines} ->
+        lines
+        |> Enum.map(fn(l) ->
+          if l.read, do: l.read, else: l.surface
+        end)
+        |> Enum.map(&String.replace(&1, "{{__}}", " "))
+        |> Enum.join
+    end
   end
-
 end
