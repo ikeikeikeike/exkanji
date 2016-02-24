2cd449a1f6968803e74c8344319081a94a97eb27
ikeikeikeike <jp.ne.co.jp@gmail.com>
2016-02-24 18:59:59 +0900

    bump version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Exkanji.Mixfile do
 
   def project do
     [app: :exkanji,
-     version: "0.2.5",
+     version: "0.2.6",
      elixir: ">= 1.0.0",
      source_url: "https://github.com/ikeikeikeike/exromaji",
      docs: [extras: ["README.md"]],
